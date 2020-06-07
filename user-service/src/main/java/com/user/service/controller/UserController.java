f25df06eac533dbeac5dfc1e4421751ba63307bb
swati.gupta02 <swati.gupta02@IRIS-CSG-2444.irissoftware.com>
2020-06-07 15:15:36 +0530

    Updated loadbalancer

--- user-service/src/main/java/com/user/service/controller/UserController.java
+++ user-service/src/main/java/com/user/service/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.user.service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,15 @@ public class UserController {
 	
 	private Environment env;
 	
+	@Autowired
+	public UserController(Environment env) {
+		this.env = env;
+	}
+	
 	@GetMapping("/status/check")
+	@LoadBalanced
 	public String check() {
-		return "user service working";
+		return "user service working" + env.getProperty("local.server.port");
 	}
 
 }
