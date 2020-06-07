31e5a365598d1f359dd51c174dd0ca8b5b597153
swati.gupta02 <swati.gupta02@IRIS-CSG-2444.irissoftware.com>
2020-06-07 16:21:17 +0530

    Zuul proxy code

---
+++ zuul-proxy-server/src/test/java/com/zuul/proxy/ZuulProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuul.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
