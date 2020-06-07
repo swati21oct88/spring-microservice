c81a5c0c0bb7e7d9edca07054cfb1a20a3798a75
swati.gupta02 <swati.gupta02@IRIS-CSG-2444.irissoftware.com>
2020-06-07 15:13:24 +0530

    Added

--- user-service/src/main/java/com/user/service/controller/UserController.java
+++ user-service/src/main/java/com/user/service/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.user.service.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+	
+	private Environment env;
 	
 	@GetMapping("/status/check")
 	public String check() {
