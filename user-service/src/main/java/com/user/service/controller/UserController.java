31e772c7da3db329d65e6f99c8f2e15d241265a1
swati.gupta02 <swati.gupta02@IRIS-CSG-2444.irissoftware.com>
2020-06-07 10:46:22 +0530

    Eureka Client

---
+++ user-service/src/main/java/com/user/service/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.user.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@GetMapping("/status/check")
+	public String check() {
+		return "user service working";
+	}
+
+}
